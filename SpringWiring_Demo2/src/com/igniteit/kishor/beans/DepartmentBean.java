0fec1d3ce615cc646e2de7a2f355810d964e14dd
kishor <kishor@172.20.10.3>
2019-06-20 21:37:01 +0530

    Simple Wiring Demo_2

---
+++ SpringWiring_Demo2/src/com/igniteit/kishor/beans/DepartmentBean.java
@@ -0,0 +1,11 @@
+package com.igniteit.kishor.beans;
+
+public class DepartmentBean {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
